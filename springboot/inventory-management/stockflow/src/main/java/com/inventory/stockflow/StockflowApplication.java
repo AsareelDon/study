77cc94c6ddde4f951ab1dac21c11b602e3691323
AsareelDon <asareeldonpena@gmail.com>
2024-08-25 22:54:21 +0800

    initialize springboot backend

---
+++ springboot/inventory-management/stockflow/src/main/java/com/inventory/stockflow/StockflowApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.stockflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowApplication.class, args);
+	}
+
+}
---
+++ springboot/inventory-management/stockflow/src/test/java/com/inventory/stockflow/StockflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.stockflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
