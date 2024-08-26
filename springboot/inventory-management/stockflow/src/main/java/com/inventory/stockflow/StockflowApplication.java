419cd710f74c1fa0c8b24fd16386cbe042083712
AsareelDon <asareeldonpena@gmail.com>
2024-08-26 16:56:29 +0800

    feat: enable auto-configuration for web server setup and REST endpoint

--- springboot/inventory-management/stockflow/src/main/java/com/inventory/stockflow/StockflowApplication.java
+++ springboot/inventory-management/stockflow/src/main/java/com/inventory/stockflow/StockflowApplication.java
@@ -1,13 +1,15 @@
 package com.inventory.stockflow;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class StockflowApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockflowApplication.class, args);
 	}
-
 }
