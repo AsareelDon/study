aff9b4e2cedd65542ddbea26aae1ad7508e898e6
AsareelDon <asareeldonpena@gmail.com>
2024-08-28 09:28:42 +0800

    feat: established unidirectional relation with multiple entity

---
+++ springboot/inventory-management/stockflow/src/main/java/com/inventory/stockflow/entity/OrderDetails.java
@@ -0,0 +1,34 @@
+package com.inventory.stockflow.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "order_details")
+public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Orders orders;
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Products products;
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "shipment_id")
+    private OrderShipment orderShipment;
+}
