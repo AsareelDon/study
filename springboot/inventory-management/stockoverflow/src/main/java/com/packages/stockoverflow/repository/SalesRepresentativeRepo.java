d3bce248e17c05f4d2bee9501717e572552d45d7
AsareelDon <asareeldonpena@gmail.com>
2024-09-02 14:32:35 +0800

    add jpa-repo for salesrep-entity

---
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/repository/SalesRepresentativeRepo.java
@@ -0,0 +1,11 @@
+package com.packages.stockoverflow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.packages.stockoverflow.entity.SalesRepresentative;
+
+@Repository
+public interface SalesRepresentativeRepo extends JpaRepository <SalesRepresentative, Long> {
+
+}
