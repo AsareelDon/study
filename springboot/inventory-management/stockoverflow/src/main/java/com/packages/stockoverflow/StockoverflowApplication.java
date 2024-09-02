9969360a5a61d581ddd5ee79bdd89c6e040cf70b
AsareelDon <asareeldonpena@gmail.com>
2024-09-02 13:37:27 +0800

    initialize project

---
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/StockoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.packages.stockoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockoverflowApplication.class, args);
+	}
+
+}
---
+++ springboot/inventory-management/stockoverflow/src/test/java/com/packages/stockoverflow/StockoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packages.stockoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
