561e67fcf3399dec054ed827489550e3c95e0a41
AsareelDon <asareeldonpena@gmail.com>
2024-09-09 12:59:39 +0800

    feat: added new service layer for salesrep retrieval base on id

--- springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/services/sales_representative/SalesRepServices.java
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/services/sales_representative/SalesRepServices.java
@@ -2,9 +2,11 @@ package com.packages.stockoverflow.services.sales_representative;
 
 import java.util.List;
 
+import com.packages.stockoverflow.dto.SalesRepresentativeDTO;
 import com.packages.stockoverflow.entity.SalesRepresentative;
 
 public interface SalesRepServices {
     SalesRepresentative createSalesRepresentative(SalesRepresentative salesRepresentative);
     List<SalesRepresentative> getAllSalesRepresentatives();
+    SalesRepresentativeDTO getSalesRepById(Long salesRepId);
 }
