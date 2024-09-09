913080790fe24faf33bd891a88dff4810ffb8ae9
AsareelDon <asareeldonpena@gmail.com>
2024-09-09 15:46:45 +0800

    feat: added new endpoint to retrieve salesrep info by requesting id

--- springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/controllers/SalesRepresentativeController.java
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/controllers/SalesRepresentativeController.java
@@ -3,6 +3,7 @@ package com.packages.stockoverflow.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.packages.stockoverflow.dto.SalesRepresentativeDTO;
 import com.packages.stockoverflow.entity.SalesRepresentative;
 import com.packages.stockoverflow.services.sales_representative.SalesRepServices;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -33,4 +35,11 @@ public class SalesRepresentativeController {
     public List<SalesRepresentative> getAllRepsAccount() {
         return salesRepServices.getAllSalesRepresentatives();
     }
+
+    @GetMapping("getSalesRepresentative/{id}")
+    public ResponseEntity<SalesRepresentativeDTO> getSalesResponseEntityById(@PathVariable("id") Long salesRepId) {
+        SalesRepresentativeDTO salesRepresentativeInfo = salesRepServices.getSalesRepById(salesRepId);
+        return new ResponseEntity<>(salesRepresentativeInfo, HttpStatus.OK);
+    }
+    
 }
