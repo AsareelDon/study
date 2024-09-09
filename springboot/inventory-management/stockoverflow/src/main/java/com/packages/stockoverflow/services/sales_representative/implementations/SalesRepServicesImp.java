72c3ccf3a43c1be8cadff05d1b6f068a241bcbcc
AsareelDon <asareeldonpena@gmail.com>
2024-09-09 12:58:59 +0800

    feat: added business logic to retrieve specific data

--- springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/services/sales_representative/implementations/SalesRepServicesImp.java
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/services/sales_representative/implementations/SalesRepServicesImp.java
@@ -1,9 +1,12 @@
-package com.packages.stockoverflow.services.sales_representative.imp;
+package com.packages.stockoverflow.services.sales_representative.implementations;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.packages.stockoverflow.dto.SalesRepresentativeDTO;
+import com.packages.stockoverflow.dto.mapper.SalesRepresentativeMapper;
 import com.packages.stockoverflow.entity.SalesRepresentative;
 import com.packages.stockoverflow.repository.SalesRepresentativeRepo;
 import com.packages.stockoverflow.services.sales_representative.SalesRepServices;
@@ -24,6 +27,12 @@ public class SalesRepServicesImp implements SalesRepServices {
     @Override
     public List<SalesRepresentative> getAllSalesRepresentatives() {
         return salesRepresentativeRepo.findAll();
+    }
+
+    @Override
+    public SalesRepresentativeDTO getSalesRepById(Long salesRepId) {
+        Optional<SalesRepresentative> salesRepInfo = salesRepresentativeRepo.findById(salesRepId);
+        return SalesRepresentativeMapper.convSalesRepresentativeToDTO(salesRepInfo.get());
     }
     
 }
