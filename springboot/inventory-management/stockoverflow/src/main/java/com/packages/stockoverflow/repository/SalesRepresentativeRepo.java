b8447074c98ff89514079293a908c81988e57d3a
AsareelDon <asareeldonpena@gmail.com>
2024-09-11 15:36:23 +0800

    feat: added email validation method for dups

--- springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/repository/SalesRepresentativeRepo.java
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/repository/SalesRepresentativeRepo.java
@@ -1,5 +1,7 @@
 package com.packages.stockoverflow.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.packages.stockoverflow.entity.SalesRepresentative;
 
 @Repository
 public interface SalesRepresentativeRepo extends JpaRepository <SalesRepresentative, Long> {
-    // public SalesRepresentative findByEmail(String email);
+    Optional<SalesRepresentative> findDuplicateEmail(String email);
 }
