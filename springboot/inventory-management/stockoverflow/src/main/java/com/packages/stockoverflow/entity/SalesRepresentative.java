e2ef4ba1f31b13072a854a0115f773ef2459d357
AsareelDon <asareeldonpena@gmail.com>
2024-09-12 13:11:57 +0800

    feat: update entity for token base auth

--- springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/entity/SalesRepresentative.java
+++ springboot/inventory-management/stockoverflow/src/main/java/com/packages/stockoverflow/entity/SalesRepresentative.java
@@ -1,17 +1,32 @@
 package com.packages.stockoverflow.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Entity(name = "SalesRep")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "sales_representative")
-public class SalesRepresentative {
+public class SalesRepresentative implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "salesrep_id", nullable = false, unique = true)
@@ -31,4 +46,42 @@ public class SalesRepresentative {
 
     @Column(name = "password", nullable = false)
     private String userPassword;
+
+    @Enumerated(EnumType.STRING)
+    private AccountRole accountRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(accountRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
